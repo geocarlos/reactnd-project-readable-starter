9ead0fbf4fe30b1424e471641a78cf4cacbfa173
Geocarlos Lopes Alves <geowildcat@gmail.com>
2018-04-09 23:10:33 -0400

    Add markup for post list

--- frontend/src/components/PostList.js
+++ frontend/src/components/PostList.js
@@ -1,17 +1,46 @@
 import React, {Component} from 'react';
+import {formatDate, uuid} from '../utils/general_functions';
 import {connect} from 'react-redux';
+import {fetchPostList} from '../actions';
 
 class PostList extends Component {
   componentDidMount(){
-    console.log("componentDidMount")
+    this.props.fetchPosts('http://localhost:3001/posts');
+    console.log(this.props.posts)
+    console.log(uuid());
   }
   render(){
+    const {posts} = this.props;
     return (
       <div className='post-list'>
         <h1>Post List</h1>
+        <ul>
+          {posts.map(post=>(
+            <li key={post.id}>
+              <h2>{post.title}</h2>
+              <p>
+                Score: {post.voteScore} | {
+                  post.commentCount} comments | {
+                    formatDate(post.timestamp)}
+              </p>
+            </li>
+          ))}
+        </ul>
       </div>
     )
   }
 }
 
-export default connect()(PostList);
+function mapStateToProps({posts}){
+  return {
+    posts
+  }
+}
+
+function mapDispatchToProps(dispatch){
+  return {
+    fetchPosts: (url) => dispatch(fetchPostList(url))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostList);
---
+++ frontend/src/utils/general_functions.js
@@ -0,0 +1,40 @@
+/** Receive a date in miliseconds and return it formatted in
+  Month, Day, Year, Hours, and Minutes
+*/
+export function formatDate(miliseconds){
+  const date = new Date(Number(miliseconds));
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ]
+  const h = date.getHours();
+  const m = date.getMinutes();
+  return `${months[date.getMonth()]}, ${date.getDate()}
+    ${date.getFullYear()}
+    - ${h < 9?'0':''}${h}:${m < 9?'0':''}${m}`;
+}
+
+/**
+  I have got this from https://gist.github.com/jcxplorer/823878,
+  and removed the line that added a dash, dividing the ID into
+  blocks.
+*/
+
+export function uuid() {
+  let uuid = "", i, random;
+  for (i = 0; i < 32; i++) {
+    random = Math.random() * 16 | 0;
+    uuid += (i === 12 ? 4 : (i === 16 ? (random & (3 | 8)) : random)).toString(16);
+  }
+  return uuid;
+}
